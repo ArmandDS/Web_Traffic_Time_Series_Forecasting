bd2983add8d3a43504a1755b1ff97b0cfeeb4142
jairsan <jairsan@inf.upv.es>
2017-07-19 16:31:59 +0200

    Add MergerUtils

---
+++ src/Merger.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+CSVfiles = [f for f in os.listdir('../partial') if os.path.isfile(f) and len(f) > 4 and f[-4:]==".csv"]
+
+
+result_df= pd.DataFrame(data={"Page":[],"Visits":[]},columns=["Page","Visits"])
+
+for f in CSVfiles:
+    aux_df = pd.read_csv(f)
+    pd.concat([result_df, aux_df], ignore_index=True)
+
+key_df = pd.read_csv("../input/key_1.csv")
+
+submission_df = pd.merge(key_df, result_df, on='Page')
+
+submission_df.to_csv(path_or_buf="../output/fbProphet_first2000TS_with_8missing_submission.csv",columns=["Id","Visits"],index=False)
