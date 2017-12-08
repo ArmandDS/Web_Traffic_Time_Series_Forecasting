4dcc4d84f1c42c8ab38d6485b4feff928c0993f3
jairsan <jairsan@inf.upv.es>
2017-12-08 11:05:42 +0100

    Add Outlier Removal

--- src/Naive_Forecast.py
+++ src/Naive_Forecast.py
@@ -16,8 +16,8 @@ plt.show()
 """
 
 #Define prediction period strings for Phase 1
-startDate = datetime.date(year=2017,month=1,day=1)
-endDate= datetime.date(year=2017,month=3,day=1)
+startDate = datetime.date(year=2017,month=9,day=13)
+endDate= datetime.date(year=2017,month=11,day=13)
 dateStrings=[]
 while startDate <=endDate:
     dateStrings.append("_" + startDate.strftime("%Y-%m-%d"))
@@ -25,7 +25,7 @@ while startDate <=endDate:
 nr_predictions=len(dateStrings)
 
 #Eliminate Nan
-train_df = pd.read_csv("../input/train_1.csv").fillna(0)
+train_df = pd.read_csv("../input/train_2.csv").fillna(0)
 
 
 print(len(train_df.index))
@@ -35,7 +35,8 @@ pageArray = []
 VisitsArray = []
 for i in range(0,len(train_df.index)):
     entry = train_df.iloc[i]
-    visits = entry[1:].median()
+    #visits = entry[1:].median()
+    visits = entry[-120:].median()
     page = entry[0]
     #For each series, we are going to write the predictions
     pageArray += [str(page+x) for x in dateStrings]
@@ -49,11 +50,11 @@ for i in range(0,len(train_df.index)):
 
 result_df= pd.DataFrame(data={"Page":pageArray,"Visits":VisitsArray},columns=["Page","Visits"])
 
-key_df = pd.read_csv("../input/key_1.csv")
+key_df = pd.read_csv("../input/key_2.csv")
 
 submission_df = pd.merge(key_df, result_df, on='Page')
 
-submission_df.to_csv(path_or_buf="../output/fbpropheTsubmission.csv",columns=["Id","Visits"],index=False)
+submission_df.to_csv(path_or_buf="../output/median2_last120.csv",columns=["Id","Visits"],index=False)
 
 
 
